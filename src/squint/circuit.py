Fix denisty operator probability calculation, test noise vmaps

src/squint/circuit.py
@@ -113,9 +113,9 @@ class Circuit(eqx.Module):
                 optimize=path,
             )
             
-        
+        backend = self.backend
         _tensor = functools.partial(
-            _tensor_func, dim=dim, subscripts=self.subscripts, path=path, backend=self.backend,
+            _tensor_func, dim=dim, subscripts=self.subscripts, path=path, backend=backend,
         )
 
         def _forward_state_func(params: PyTree, static: PyTree):
@@ -124,8 +124,16 @@ class Circuit(eqx.Module):
 
         _forward_state = functools.partial(_forward_state_func, static=static)
 
-        def _forward_prob(params: PyTree):
-            return jnp.abs(_forward_state(params)) ** 2
+
+        if backend == 'unitary':
+            def _forward_prob(params: PyTree):
+                return jnp.abs(_forward_state(params)) ** 2
+            
+        elif backend == 'nonunitary':
+            def _forward_prob(params: PyTree):
+                _subscripts_tmp = [get_symbol(i) for i in range(len(self.wires))]
+                _subscripts = "".join(_subscripts_tmp + _subscripts_tmp) + "->" + "".join(_subscripts_tmp)
+                return jnp.abs(jnp.einsum(_subscripts, _forward_state(params)))
 
         _grad_state_nonholomorphic = jax.jacrev(_forward_state, holomorphic=True)
 

src/squint/ops/noise.py
@@ -14,7 +14,8 @@ from rich.pretty import pprint
 import polars as pl
 from beartype import beartype
 from opt_einsum.parser import get_symbol
-
+from jaxtyping import ArrayLike
+import paramax 
 from squint.ops.base import AbstractChannel, basis_operators, AbstractGate, AbstractState
 from squint.ops.dv import DiscreteState, XGate, ZGate
 from squint.circuit import Circuit
@@ -23,7 +24,7 @@ from squint.utils import print_nonzero_entries
 
 
 class BitFlipChannel(AbstractChannel):
-    p: float 
+    p: ArrayLike 
     
     @beartype
     def __init__(
@@ -32,7 +33,8 @@ class BitFlipChannel(AbstractChannel):
         p: float
     ):
         super().__init__(wires=wires)
-        self.p = p  #paramax.non_trainable(p)
+        self.p = jnp.array(p)
+        # self.p = p  #paramax.non_trainable(p)
         return
 
     def __call__(self, dim: int):
@@ -41,5 +43,30 @@ class BitFlipChannel(AbstractChannel):
             [
                 jnp.sqrt(1-self.p) * basis_operators(dim=2)[3],   # identity
                 jnp.sqrt(self.p) * basis_operators(dim=2)[2],     # X
+            ]
+        )
+        
+
+class DepolarizingChannel(AbstractChannel):
+    p: ArrayLike 
+    
+    @beartype
+    def __init__(
+        self,
+        wires: tuple[int],
+        p: float
+    ):
+        super().__init__(wires=wires)
+        self.p = jnp.array(p)
+        return
+
+    def __call__(self, dim: int):
+        assert dim == 2
+        return jnp.array(
+            [
+                jnp.sqrt(1-self.p) * basis_operators(dim=2)[3],   # identity
+                jnp.sqrt(self.p/3) * basis_operators(dim=2)[0],     # Z
+                jnp.sqrt(self.p/3) * basis_operators(dim=2)[1],     # Y
+                jnp.sqrt(self.p/3) * basis_operators(dim=2)[2],     # X
             ]
         )